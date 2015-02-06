40f30951fada5afff3e29e36447ef7bf9278b901
shahanak <shahana.k@experionglobal.com>
2015-02-06 12:12:19 +0530

    schedule City

---
+++ scheduleCity/public/ui/main/main.js
@@ -0,0 +1,39 @@
+angular.module('scheduleCity').controller('MainCtrl',function($scope,$firebase){	
+ var ref = new window.Firebase("https://blinding-heat-4724.firebaseio.com/days");  
+  var fb = $firebase(ref);
+   $scope.reset = function() {    
+
+    fb.$set({
+      monday: {
+        name: 'Monday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          }
+        }
+      },
+      tuesday: {
+        name: 'Tuesday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          }
+        }
+      }
+    });    
+
+  };
+  var syncObject = fb.$asObject();
+  syncObject.$bindTo($scope, 'days');
+
+});
